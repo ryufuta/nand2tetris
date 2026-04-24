544d1f1bf0d417910ba3efc10aee25987d98a6ab
ryufuta <rfutago@gmail.com>
2026-04-24 22:05:48 +0900

    テストスクリプト追加

---
+++ test_basic.sh
@@ -0,0 +1,4 @@
+./basic/HackAssembler ./data/Add.asm
+./basic/HackAssembler ./data/MaxL.asm
+./basic/HackAssembler ./data/RectL.asm
+./basic/HackAssembler ./data/PongL.asm
---
+++ test_final.sh
@@ -0,0 +1,7 @@
+./final/HackAssembler ./data/Add.asm
+./final/HackAssembler ./data/MaxL.asm
+./final/HackAssembler ./data/RectL.asm
+./final/HackAssembler ./data/PongL.asm
+./final/HackAssembler ./data/Max.asm
+./final/HackAssembler ./data/Rect.asm
+./final/HackAssembler ./data/Pong.asm
